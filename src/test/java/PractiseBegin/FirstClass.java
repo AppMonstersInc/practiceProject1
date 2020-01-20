f4c828d3901a375d206a7c999e87211d80402863
Jack Steven <jurabek.sayfiev2@gmail.com>
2020-01-20 17:12:47 -0600

    initial commit

---
+++ src/test/java/PractiseBegin/FirstClass.java
@@ -0,0 +1,12 @@
+package PractiseBegin;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver  = new ChromeDriver();
+    }
+}
