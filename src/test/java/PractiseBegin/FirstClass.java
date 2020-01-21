a29c4ec6cfd8a9aaec0f9570094e20864a2700c6
Jack Steven <jurabek.sayfiev2@gmail.com>
2020-01-20 18:04:00 -0600

    Merge branch 'master' of C:\Users\Jurabek\Desktop\AutomationPractiseMyself with conflicts.

--- src/test/java/PractiseBegin/FirstClass.java
+++ src/test/java/PractiseBegin/FirstClass.java
@@ -9,7 +9,6 @@ public class FirstClass {
         WebDriverManager.chromedriver().setup();
         WebDriver driver  = new ChromeDriver();
         System.out.println("change");
-        System.out.println("changed from github UI");
-        driver.get("http://google.com");
+        System.out.println("practice stash");
     }
 }
